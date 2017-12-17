a2daa8ba0ac11d576d3ec0f300f79e4011a005e3
Dell <wasn.alriyami@gmail.com>
2017-12-18 00:35:18 +0400

    Home_page

--- app/src/main/java/com/example/sasmob/firebaseauthdemo/Home_page.java
+++ app/src/main/java/com/example/sasmob/firebaseauthdemo/Home_page.java
@@ -1,13 +1,47 @@
 package com.example.sasmob.firebaseauthdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Home_page extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Home_page extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth firebaseAuth;
+    private TextView userEmail;
+    private Button logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser()== null){
+            finish();
+            startActivity(new Intent(this,SignIn.class));
+        }
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        userEmail = (TextView) findViewById(R.id.textView);
+        userEmail.setText("Welcome" + user.getEmail());
+        logout = (Button) findViewById(R.id.btn_logout);
+
+        logout.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if ( view == logout){
+            firebaseAuth.signOut();
+            startActivity(new Intent(this,SignIn.class));
+        }
     }
 }
